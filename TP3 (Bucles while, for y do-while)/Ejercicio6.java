07ce8524063a73b2a5ba00b35d3d5e52b215f05e
Octavio884 <octaviopietronave1@gmail.com>
2026-04-23 11:49:17 -0300

    Corrección
    r

--- TP3 (Bucles while, for y do-while)/Ejercicio6.java
+++ TP3 (Bucles while, for y do-while)/Ejercicio6.java
@@ -8,8 +8,12 @@ public class Ejercicio6{
             System.out.println("Ingrese N y M, ambos positivos");
             N=objeto.nextInt();
             M=objeto.nextInt();
-            if (N>=M){
-                System.out.println("N debe ser menor a M");
+            if (N>=M || N<=0 || M<=0){
+                System.out.println("N debe ser menor a M y ambos numeros deben ser positivos");
+                continue;
+            }
+            for (int i=1; N*i<=M; i++){
+                    System.out.println(N*i);
             }
         } while(M<N || N<=0 || M<=0);
     }
